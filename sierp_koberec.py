a2b68f990d157b9e2cd1a58480619fc5e9bb1acb
bartelka <tanicka879gmail.com>
2023-09-13 18:22:56 +0200

    dorobena du

--- sierp_koberec.py
+++ sierp_koberec.py
@@ -1,18 +1,33 @@
 import tkinter as tk
 win = tk.Tk()
 
-def sierp(a,b,d,hlbka=12):
+def sierp(a,b,d,hlbka=6):
    if hlbka > 0:
         hlbka -= 1
         canvas.create_rectangle(a,b,a+d,b+d)
-        
+        canvas.create_rectangle(a,b,a+d//3,b+d//3)
+        canvas.create_rectangle(a+d//3,b,a+2*d//3,b+d//3)
+        canvas.create_rectangle(a+2*d//3,b,a+d,b+d//3)
+        canvas.create_rectangle(a,b+d//3,a+d//3,b+2*d//3)
+        canvas.create_rectangle(a+2*d//3,b+d//3,a+d,b+2*d//3)
+        canvas.create_rectangle(a,b+2*d//3,a+d//3,b+d)
+        canvas.create_rectangle(a+d//3,b+2*d//3,a+2*d//3,b+d)
+        canvas.create_rectangle(a+2*d//3,b+2*d//3,a+d,b+d)
+        sierp(a,b,d//3,hlbka)
+        sierp(a+d//3,b,d//3,hlbka)
+        sierp(a+2*d//3,b,d//3,hlbka)
+        sierp(a,b+d//3,d//3,hlbka)
+        sierp(a+2*d//3,b+d//3,d//3,hlbka)
+        sierp(a,b+2*d//3,d//3,hlbka)
+        sierp(a+d//3,b+2*d//3,d//3,hlbka)
+        sierp(a+2*d//3,b+2*d//3,d//3,hlbka)
 
-h = 1000
-w = 1000
-d = 900
+h = 800
+w = 800
+d = 781
 
 canvas = tk.Canvas(win, height=h, width=w, bg="white")
 canvas.pack()
 
-sierp(50,50,d)
+sierp(10,10,d)
 win.mainloop()
